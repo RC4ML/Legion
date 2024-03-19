d28fdc1b2e4102c40188e0c383f29c83d1a6861a
jiesun233 <jiesun@zju.edu.cn>
2024-03-19 19:22:19 +0800

    enable evaluation

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd sampling_server && \
 make clean && make -j 8 && \
-cd .. 
-# cd training_backend && \
-# python setup.py install && \ 
-# cd ..
+cd .. && \
+cd training_backend && \
+python setup.py install && \ 
+cd ..
